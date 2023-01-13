4a89a5922706fe444e15f374166cdedb6d217542
David Thumbi <davidthumbi78@gmail.com>
2023-01-13 18:44:46 +0300

    Initial Commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        "url":"https://v16-webapp.tiktok.com/a743616ccfd4fb6081d1089ea9ad992d/63c0418e/video/tos/maliva/tos-maliva-ve-0068c799-us/2a54b2745b2d44ec8e88d502962d849f/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=4046&bt=2023&cs=0&ds=3&ft=4b~OyM9x8Zmo0v3iW64jVGwyJpWrKsdm&mime_type=video_mp4&qs=0&rc=OWlnO2gzaGRpaTdpNDxoZEBpajg6OTM6ZjVnZzMzZzczNEA0NDZfLWFeX18xMjBjMV42YSNjNmNhcjRnc3JgLS1kMS9zcw%3D%3D&l=2023011211210687DC62C2FE300C089FAB&btag=80000",
+        "channel":"foodies",
+        "description":"Preparation of eggs and bread.",
+        "song":"99 problems but react aint one - Jayz ft Sonny",
+        "likes":"123",
+        "messages":"34",
+        "shares":"12",
+    },
+    {
+        "url":"https://v16-webapp.tiktok.com/06c84597163d4a05e61b8a27139cec9c/63c0613a/video/tos/useast2a/tos-useast2a-ve-0068c003/osnbGDHgWoWpreeEiJubVcvwAMxuBRQvMjQiAB/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1352&bt=676&cs=0&ds=3&ft=4b~OyM9x8Zmo0MZ9y64jVcf0JpWrKsdm&mime_type=video_mp4&qs=0&rc=ZzdnNGVoPDdmNGk7NDpmM0BpamtydTY6ZmQ1aDMzNzczM0AwYjJfYmE0NWIxXjZiYzFeYSNfYGFvcjRvX2dgLS1kMTZzcw%3D%3D&l=20230112133126F356D60BF9D135171C14&btag=80000",
+        "channel":"foodies",
+        "description":"Preparation of eggs and bread.",
+        "song":"99 problems but react aint one - Jayz ft Sonny",
+        "likes":"123",
+        "messages":"34",
+        "shares":"12",
+    }
+]
+
+export default data;
---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import Data from "./data.js"
+import Videos from "./dbModel.js";
+
+// APP CONFIG
+// create instance of app
+const app = express();
+
+// define the port the app will listen on
+const port = 8000;
+
+// MIDDLEWARES
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeaders("Access-Control-Allow-Origin", "*");
+    res.setHeaders("Access-Control-Allow-Headers", "*");
+    next();
+})
+
+
+// DB CONFIG
+const connection_url = `mongodb+srv://admin:KIprsecZVUCLgOPM@cluster0.ygnveuj.mongodb.net/?retryWrites=true&w=majority`;
+
+  mongoose.connect(connection_url);
+
+// API ENDPOINTS
+// health check api request 
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get('/v1/posts', (req, res) => res.status(200).send(Data))
+
+// will let us add a video doc to the videos collection
+app.post("/v2/posts", (req, res) => {
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err.message)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+// will get a list of all the videos from the database
+app.get("/v2/posts", (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err.message)
+        }else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+// listen on port
+app.listen(port, () => console.log(`listening on localhost:${port}`));
